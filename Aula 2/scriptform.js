91b9a1d674b8bac4291c8876c560d349941c341a
Gabriel Lima <gabrilima2004@gmail.com>
2024-08-12 10:39:46 -0300

    Recepção dos 3 inputs do formulário

--- Aula 2/scriptform.js
+++ Aula 2/scriptform.js
@@ -1,3 +1,14 @@
-let dom = document;
+let botao = document.getElementById("funcao-botao");
 
-console.log(dom)
+botao.addEventListener("click", enviar);
+
+//console.log(dom);
+
+function enviar() {
+    let Nome = document.getElementById("Nome").value;
+    let Idade = document.getElementById("Idade").value;
+    let CPF = document.getElementById("CPF").value;
+
+    console.log(Nome, Idade, CPF)
+    alert("OK!");
+}
