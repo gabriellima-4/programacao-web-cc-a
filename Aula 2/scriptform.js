8daffee8cbf64c48ee4e99f70d6bbcf1c0ec7b6f
Gabriel Lima <gabrilima2004@gmail.com>
2024-08-15 14:31:17 -0300

    Regex faltando

--- Aula 2/scriptform.js
+++ Aula 2/scriptform.js
@@ -9,6 +9,24 @@ function enviar() {
     let Idade = document.getElementById("Idade").value;
     let Email = document.getElementById("Email").value;
 
-    console.log(Nome, Idade, Email)
-    alert("OK!");
+    function FiltroEmail() {
+        const regexEmail = ""; //vou procurar e já volto
+        return regexEmail.test(Email);
+    }
+
+    const ValidacaoEmail = (Email) => {
+        if (!FiltroEmail(Email)) {
+            alert("Este não é um email válido!")
+            return false;
+        }
+
+        return true;
+    }
+
+    if (!ValidacaoEmail(Email)) {
+        return;
+    }
+    
+    console.log(Nome, Idade, Email);
+    alert("Usuário confirmado! Bem-vindo(a)!");
 }
